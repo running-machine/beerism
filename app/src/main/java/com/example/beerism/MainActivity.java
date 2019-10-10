0e8ecc2612e111091e088015a3ac124f2d78e40e
chimaek <pipiru100@gmail.com>
2019-10-10 17:28:21 +0900

    파이어스토어 db생성

--- app/src/main/java/com/example/beerism/MainActivity.java
+++ app/src/main/java/com/example/beerism/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.beerism;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -16,20 +17,21 @@ import com.example.beerism.Fragment.GS_Fragment;
 import com.example.beerism.Fragment.SevenEleven_Fragment;
 import com.gigamole.navigationtabstrip.NavigationTabStrip;
 import com.github.clans.fab.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class MainActivity extends AppCompatActivity {
     NavigationTabStrip main_nts;
     ViewPager main_vp;
     FloatingActionButton fab_detetion, fab_search, fab_rec;
-
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(getApplicationContext(), firebaseFirestore.collection("beer").document("cass").get(),Toast.LENGTH_SHORT).show();
         fab_detetion = findViewById(R.id.menu_item_detect);
         fab_rec = findViewById(R.id.menu_item_recommand);
         fab_search = findViewById(R.id.menu_item_search);
