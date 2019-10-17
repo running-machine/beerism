b90b129ed071cf997584f1b56becc24390430568
chimaek <pipiru100@gmail.com>
2019-10-17 10:09:33 +0900

    코드정리

--- app/src/main/java/com/example/beerism/MainActivity.java
+++ app/src/main/java/com/example/beerism/MainActivity.java
@@ -63,8 +63,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
 //        rootLayout = findViewById(R.id.coordinatorLayout);
 //        menuLayout = (ClipRevealFrame) findViewById(R.id.menu_layout);
 //        arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
@@ -109,257 +107,145 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeDialog() {
-
-        AlertDialog.Builder alt_bld = new AlertDialog.Builder(MainActivity.this,R.style.Alert);
-
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(MainActivity.this, R.style.Alert);
         alt_bld.setTitle("사진 업로드").setIcon(R.drawable.object_detection).setCancelable(
-
                 false).setPositiveButton("사진촬영",
-
                 new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
-
                         // 사진 촬영 클릭
-
                         Log.v("알림", "다이얼로그 > 사진촬영 선택");
-
                         takePhoto();
-
                     }
 
                 }).setNeutralButton("앨범선택",
-
                 new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialogInterface, int id) {
-
                         Log.v("알림", "다이얼로그 > 앨범선택 선택");
-
                         //앨범에서 선택
-
                         selectAlbum();
-
                     }
 
                 }).setNegativeButton("취소   ",
-
                 new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int id) {
-
                         Log.v("알림", "다이얼로그 > 취소 선택");
-
                         // 취소 클릭. dialog 닫기.
-
                         dialog.cancel();
-
                     }
 
                 });
-
         AlertDialog alert = alt_bld.create();
-
         alert.show();
-
     }
 
-
     //사진 찍기 클릭
-
     public void takePhoto() {
-
         // 촬영 후 이미지 가져옴
-
         String state = Environment.getExternalStorageState();
-
         //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-
         if (Environment.MEDIA_MOUNTED.equals(state)) {
-
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
             if (intent.resolveActivity(getPackageManager()) != null) {
-
                 File photoFile = null;
-
                 try {
-
                     photoFile = createImageFile();
-
                 } catch (IOException e) {
-
                     e.printStackTrace();
-
                 }
-
                 if (photoFile != null) {
-
-                    Uri providerURI = FileProvider.getUriForFile(this,"com.example.beerism.fileprovider", photoFile);
-
+                    Uri providerURI = FileProvider.getUriForFile(this, "com.example.beerism.fileprovider", photoFile);
                     imgUri = providerURI;
-
                     intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, providerURI);
-
                     startActivityForResult(intent, FROM_CAMERA);
-
                 }
-
             }
-
         } else {
             Log.v("알림", "저장공간에 접근 불가능");
             return;
         }
-
     }
 
 
     public File createImageFile() throws IOException {
-
         String imgFileName = System.currentTimeMillis() + ".jpg";
-
         File imageFile = null;
-
-        File storageDir = new File(Environment.getExternalStorageDirectory()+ "/Pictures" ,"file");
-
-
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "file");
         if (!storageDir.exists()) {
-
             Log.v("알림", "storageDir 존재 x " + storageDir.toString());
-
             storageDir.mkdirs();
-
         }
-
         Log.v("알림", "storageDir 존재함 " + storageDir.toString());
-
         imageFile = new File(storageDir, imgFileName);
-
         mCurrentPhotoPath = imageFile.getAbsolutePath();
-
-
         return imageFile;
-
     }
 
-
     //앨범 선택 클릭
-
     public void selectAlbum() {
-
         //앨범에서 이미지 가져옴
-
-
         //앨범 열기
-
         Intent intent = new Intent(Intent.ACTION_PICK);
-
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-
-
         intent.setType("image/*");
-
-
         startActivityForResult(intent, FROM_ALBUM);
-
     }
 
 
     public void galleryAddPic() {
-
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-
         File f = new File(mCurrentPhotoPath);
-
         Uri contentUri = Uri.fromFile(f);
-
         mediaScanIntent.setData(contentUri);
-
         sendBroadcast(mediaScanIntent);
-
         Toast.makeText(this, "사진이 저장되었습니다", Toast.LENGTH_SHORT).show();
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         super.onActivityResult(requestCode, resultCode, data);
-
-
         if (resultCode != RESULT_OK) {
-
             return;
-
         }
-
         switch (requestCode) {
-
             case FROM_ALBUM: {
-
                 //앨범에서 가져오기
-
                 if (data.getData() != null) {
-
                     try {
-
                         File albumFile = null;
 
                         albumFile = createImageFile();
 
-
                         photoURI = data.getData();
-
                         albumURI = Uri.fromFile(albumFile);
-
-
                         galleryAddPic();
-
                         img1.setImageURI(photoURI);
-
                         //cropImage();
 
                     } catch (Exception e) {
-
                         e.printStackTrace();
-
                         Log.v("알림", "앨범에서 가져오기 에러");
-
                     }
-
                 }
-
                 break;
-
             }
 
             case FROM_CAMERA: {
-
                 //카메라 촬영
-
                 try {
-
                     Log.v("알림", "FROM_CAMERA 처리");
-
                     galleryAddPic();
-
                     img1.setImageURI(imgUri);
 
                 } catch (Exception e) {
-
                     e.printStackTrace();
-
                 }
-
                 break;
-
             }
-
         }
-
     }
 
 
